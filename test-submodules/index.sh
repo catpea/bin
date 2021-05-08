2d30668f22a6d56c6db6f2d23f730847a1770415
catpea <corporate.miou@gmail.com>
2021-05-08 12:33:38 -0400

    New Bin Revisions

---
+++ test-submodules/index.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+ROOT='.';
+if [ ! -d $ROOT ]; then
+  mkdir -p $ROOT;
+fi;
+
+shopt -s lastpipe; # If set, and job control is not active, the shell runs the last command of a pipeline not executed in the background in the current shell environment. This allows simple access to file contents.
+(sed -e 's/#.*$//' -e '/^$/d' -e 's/[[:space:]]*$//' .submodules.txt) | readarray -t submodules
+
+for ix in ${!submodules[*]}
+do
+  url=${submodules[$ix]};
+  name=$(basename -s .git $url)
+  target="${ROOT}/${name}";
+  echo "($(expr ${ix} + 1)/${#submodules[*]}) $name: $url -> $target";
+
+  if [ -d $target ]; then
+    cd $target;
+    if [[ -z $(git status -s) ]]; then
+      echo "[OK] ${name} is in sync."
+    else
+      echo "[NO] ${name} has uncommited changes."
+      echo "SOLUTION:";
+      echo "./publish.sh"
+      exit;
+    fi
+
+    cd -
+  fi
+
+done
