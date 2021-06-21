42968bac8648878d90b2239583d4737bafbafd2a
catpea <corporate.miou@gmail.com>
2021-06-20 23:11:34 -0400

    New Bin Scripts Revisions

--- audio-to-video/index.sh
+++ audio-to-video/index.sh
@@ -28,7 +28,7 @@ for source in $(ls -1 ./furkies-purrkies/*/files/poetry-*.mp3 | sort -r); do
   if [ "$execute" = true ] ; then
     echo creating: "${destination}";
     # ffmpeg -hide_banner -loglevel panic -y -i "${source}" -filter_complex "[0:a]showspectrum=s=1280x760:slide=scroll:mode=separate:color=rainbow:scale=5thrt:win_func=sine:orientation=horizontal:legend=true,format=yuv420p,crop=1562:878:0:0,split=4[a][b][c][d],[a]waveform[aa],[b][aa]vstack[V],[c]waveform=m=0[cc],[d]vectorscope=color4[dd],[cc][dd]vstack[V2],[V][V2]hstack[v]" -map "[v]" -map 0:a "${destination}"
-    ffmpeg -y -i "${source}" -filter_complex "[0:a]showspectrum=s=1280x760:slide=scroll:mode=separate:color=plasma:scale=5thrt:win_func=sine:orientation=horizontal:legend=true,format=yuv420p,crop=1562:878:0:0[v]" -map "[v]" -map 0:a "${destination}"
+    ffmpeg -hide_banner -y -i "${source}" -filter_complex "[0:a]showspectrum=s=1280x760:slide=scroll:mode=separate:color=plasma:scale=5thrt:win_func=sine:orientation=horizontal:legend=true,format=yuv420p,crop=1562:878:0:0[v]" -map "[v]" -map 0:a "${destination}"
   fi;
 
 
