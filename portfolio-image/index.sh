2c7724fd39d120f2cc7248a146a01962ee145abf
catpea <corporate.miou@gmail.com>
2022-03-20 10:57:33 -0400

    New Bin Scripts Revisions

---
+++ portfolio-image/index.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+montage ./dist/furkies-purrkies/image/lg-poetry-{0748,0707,0704,0716,0713,0724,0714,0720,0740}-illustration.jpg -tile 3x3 -geometry 500x500 dist/furkies-purrkies/wwwroot/portfolio.jpg
