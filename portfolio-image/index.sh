ba107089fd89e91329faa99f5fad6c73953d6536
catpea <corporate.miou@gmail.com>
2022-04-20 23:50:16 -0400

    New Bin Scripts Revisions

--- portfolio-image/index.sh
+++ portfolio-image/index.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
-montage ./dist/furkies-purrkies/image/lg-poetry-{0748,0707,0704,0716,0713,0724,0714,0720,0740}-illustration.jpg -tile 3x3 -geometry 500x500 dist/furkies-purrkies/wwwroot/portfolio.jpg
+## 9 montage ./dist/furkies-purrkies/image/lg-poetry-{0748,0707,0704,0716,0713,0724,0714,0720,0740}-illustration.jpg -tile 3x3 -geometry 500x500 dist/furkies-purrkies/wwwroot/portfolio.jpg
+
+# 16 - left out punxy
+montage ./dist/furkies-purrkies/image/lg-poetry-{0695,0707,0713,0714,0716,0720,0724,0740,0746,0748,0756,0759,0764,0765,0767,0769}-illustration.jpg -tile 4x4 -geometry 500x500 dist/furkies-purrkies/wwwroot/portfolio.jpg
+
+# - means private, non-minus - rdf
+# -695
+# -704
+# -707
+# -713
+# 714
+# -716
+# 720
+# -724
+# 740
+# 746
+# 748
+# 756
+# -759
+# -764
+# -765
+# -767
+# 769
