482d88ebbcaa53dce95293db4e69c9598650a425
catpea <corporate.miou@gmail.com>
2022-04-25 13:52:09 -0400

    New Bin Scripts Revisions

--- portfolio-image/index.sh
+++ portfolio-image/index.sh
@@ -2,7 +2,11 @@
 ## 9 montage ./dist/furkies-purrkies/image/lg-poetry-{0748,0707,0704,0716,0713,0724,0714,0720,0740}-illustration.jpg -tile 3x3 -geometry 500x500 dist/furkies-purrkies/wwwroot/portfolio.jpg
 
 # 16 - left out punxy
-montage ./dist/furkies-purrkies/image/lg-poetry-{0695,0707,0713,0714,0716,0720,0724,0740,0746,0748,0756,0759,0764,0765,0767,0769}-illustration.jpg -tile 4x4 -geometry 500x500 dist/furkies-purrkies/wwwroot/portfolio.jpg
+# montage ./dist/furkies-purrkies/image/lg-poetry-{0695,0707,0713,0714,0716,0720,0724,0740,0746,0748,0756,0759,0764,0765,0767,0769}-illustration.jpg -tile 4x4 -geometry 500x500 dist/furkies-purrkies/wwwroot/portfolio.jpg
+montage ./dist/furkies-purrkies/image/lg-poetry-{0707,0713,0714,0716,0720,0724,0740,0746,0748,0756,0759,0764,0765,0767,0769,0782}-illustration.jpg -tile 4x4 -geometry 500x500 dist/furkies-purrkies/wwwroot/portfolio.jpg
+
+# BRING BACK CAT AND GROUNDHOG, ADD WITCH
+
 
 # - means private, non-minus - rdf
 # -695
